04adec3d64f11ed5b7d9c0744232574bc5248796
Vitor Mattos <vitor@php.rio>
2020-03-05 09:27:52 -0300

    Fix ident

--- ScieloDeployment.inc.php
+++ ScieloDeployment.inc.php
@@ -2,14 +2,14 @@
 
 import('lib.pkp.classes.plugins.importexport.PKPImportExportDeployment');
 
-class ScieloDeployment extends PKPImportExportDeployment {
-
-	/**
-	 * Constructor
-	 * @param $context Context
-	 * @param $user User
-	 */
-	function __construct($context, $user) {
-		parent::__construct($context, $user);
-	}
+class ScieloDeployment extends PKPImportExportDeployment
+{
+    /**
+     * Constructor
+     * @param $context Context
+     * @param $user User
+     */
+    function __construct($context, $user) {
+        parent::__construct($context, $user);
+    }
 }
